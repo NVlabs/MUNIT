c82addac5d326cde4c59d7f4c355c09537d86ec8
Xun Huang <xunh@nvidia.com>
2018-04-15 16:05:12 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,7 @@
+"""
+Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
+Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+"""
 import torch.utils.data as data
 import os.path
 
