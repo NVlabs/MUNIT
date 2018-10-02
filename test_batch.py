fb987a134476e28241ab62df5f114480fbc5de15
Xun Huang <xunhuang1995@gmail.com>
2018-10-02 15:33:19 -0400

    add code for computing Inception Score/Conditional Inception Score

--- test_batch.py
+++ test_batch.py
@@ -3,11 +3,15 @@ Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
 from __future__ import print_function
-from utils import get_config, get_data_loader_folder, pytorch03_to_pytorch04
+from utils import get_config, get_data_loader_folder, pytorch03_to_pytorch04, load_inception
 from trainer import MUNIT_Trainer, UNIT_Trainer
+from torch import nn
+from scipy.stats import entropy
+import torch.nn.functional as F
 import argparse
 from torch.autograd import Variable
 from data import ImageFolder
+import numpy as np
 import torchvision.utils as vutils
 try:
     from itertools import izip as zip
@@ -30,6 +34,10 @@ parser.add_argument('--synchronized', action='store_true', help="whether use syn
 parser.add_argument('--output_only', action='store_true', help="whether use synchronized style code or not")
 parser.add_argument('--output_path', type=str, default='.', help="path for logs, checkpoints, and VGG model weight")
 parser.add_argument('--trainer', type=str, default='MUNIT', help="MUNIT|UNIT")
+parser.add_argument('--compute_IS', action='store_true', help="whether to compute Inception Score or not")
+parser.add_argument('--compute_CIS', action='store_true', help="whether to compute Conditional Inception Score or not")
+parser.add_argument('--inception_a', type=str, default='.', help="path to the pretrained inception network for domain A")
+parser.add_argument('--inception_b', type=str, default='.', help="path to the pretrained inception network for domain B")
 
 opts = parser.parse_args()
 
@@ -40,6 +48,15 @@ torch.cuda.manual_seed(opts.seed)
 # Load experiment setting
 config = get_config(opts.config)
 input_dim = config['input_dim_a'] if opts.a2b else config['input_dim_b']
+
+# Load the inception networks if we need to compute IS or CIIS
+if opts.compute_IS or opts.compute_IS:
+    inception = load_inception(opts.inception_b) if opts.a2b else load_inception(opts.inception_a)
+    # freeze the inception models and set eval mode
+    inception.eval()
+    for param in inception.parameters():
+        param.requires_grad = False
+    inception_up = nn.Upsample(size=(299, 299), mode='bilinear')
 
 # Setup model and data loader
 image_names = ImageFolder(opts.input_folder, transform=None, return_paths=True)
@@ -68,10 +85,18 @@ trainer.eval()
 encode = trainer.gen_a.encode if opts.a2b else trainer.gen_b.encode # encode function
 decode = trainer.gen_b.decode if opts.a2b else trainer.gen_a.decode # decode function
 
+if opts.compute_IS:
+    IS = []
+    all_preds = []
+if opts.compute_CIS:
+    CIS = []
+
 if opts.trainer == 'MUNIT':
     # Start testing
     style_fixed = Variable(torch.randn(opts.num_style, style_dim, 1, 1).cuda(), volatile=True)
     for i, (images, names) in enumerate(zip(data_loader, image_names)):
+        if opts.compute_CIS:
+            cur_preds = []
         print(names[1])
         images = Variable(images.cuda(), volatile=True)
         content, _ = encode(images)
@@ -80,15 +105,39 @@ if opts.trainer == 'MUNIT':
             s = style[j].unsqueeze(0)
             outputs = decode(content, s)
             outputs = (outputs + 1) / 2.
+            if opts.compute_IS or opts.compute_CIS:
+                pred = F.softmax(inception(inception_up(outputs)), dim=1).cpu().data.numpy()  # get the predicted class distribution
+            if opts.compute_IS:
+                all_preds.append(pred)
+            if opts.compute_CIS:
+                cur_preds.append(pred)
             # path = os.path.join(opts.output_folder, 'input{:03d}_output{:03d}.jpg'.format(i, j))
             basename = os.path.basename(names[1])
             path = os.path.join(opts.output_folder+"_%02d"%j,basename)
             if not os.path.exists(os.path.dirname(path)):
                 os.makedirs(os.path.dirname(path))
             vutils.save_image(outputs.data, path, padding=0, normalize=True)
+        if opts.compute_CIS:
+            cur_preds = np.concatenate(cur_preds, 0)
+            py = np.sum(cur_preds, axis=0)  # prior is computed from outputs given a specific input
+            for j in range(cur_preds.shape[0]):
+                pyx = cur_preds[j, :]
+                CIS.append(entropy(pyx, py))
         if not opts.output_only:
             # also save input images
             vutils.save_image(images.data, os.path.join(opts.output_folder, 'input{:03d}.jpg'.format(i)), padding=0, normalize=True)
+    if opts.compute_IS:
+        all_preds = np.concatenate(all_preds, 0)
+        py = np.sum(all_preds, axis=0)  # prior is computed from all outputs
+        for j in range(all_preds.shape[0]):
+            pyx = all_preds[j, :]
+            IS.append(entropy(pyx, py))
+
+    if opts.compute_IS:
+        print("Inception Score: {}".format(np.exp(np.mean(IS))))
+    if opts.compute_CIS:
+        print("conditional Inception Score: {}".format(np.exp(np.mean(CIS))))
+
 elif opts.trainer == 'UNIT':
     # Start testing
     for i, (images, names) in enumerate(zip(data_loader, image_names)):
