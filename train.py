17b7c02263a19855f786e90ae6d9714988a37626
pegahk <pegah@elementai.com>
2019-08-05 12:44:45 -0400

    implement highway norm

--- train.py
+++ train.py
@@ -5,9 +5,10 @@ Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses
 from utils import get_all_data_loaders, prepare_sub_folder, write_html, write_loss, get_config, write_2images, Timer
 import argparse
 from torch.autograd import Variable
-from trainer import MUNIT_Trainer, UNIT_Trainer
+from trainer import MUNIT_Trainer
 import torch.backends.cudnn as cudnn
 import torch
+import datetime
 try:
     from itertools import izip as zip
 except ImportError: # will be 3.x series
@@ -16,6 +17,7 @@ import os
 import sys
 import tensorboardX
 import shutil
+import pdb
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, default='configs/edges2handbags_folder.yaml', help='Path to the config file.')
@@ -39,31 +41,49 @@ elif opts.trainer == 'UNIT':
     trainer = UNIT_Trainer(config)
 else:
     sys.exit("Only support MUNIT|UNIT")
+r = 50
 trainer.cuda()
 train_loader_a, train_loader_b, test_loader_a, test_loader_b = get_all_data_loaders(config)
-train_display_images_a = torch.stack([train_loader_a.dataset[i] for i in range(display_size)]).cuda()
-train_display_images_b = torch.stack([train_loader_b.dataset[i] for i in range(display_size)]).cuda()
-test_display_images_a = torch.stack([test_loader_a.dataset[i] for i in range(display_size)]).cuda()
-test_display_images_b = torch.stack([test_loader_b.dataset[i] for i in range(display_size)]).cuda()
+train_display_images_a = torch.stack([train_loader_a.dataset[i+r][0] for i in range(display_size)]).cuda()
+train_display_images_b = torch.stack([train_loader_b.dataset[i+r][0] for i in range(display_size)]).cuda()
+test_display_images_a = torch.stack([test_loader_a.dataset[i+r][0] for i in range(display_size)]).cuda()
+test_display_images_b = torch.stack([test_loader_b.dataset[i+r][0] for i in range(display_size)]).cuda()
+
+train_display_segs_a = torch.stack([train_loader_a.dataset[i+r][1] for i in range(display_size)]).cuda()
+train_display_segs_b = torch.stack([train_loader_b.dataset[i+r][1] for i in range(display_size)]).cuda()
+test_display_segs_a = torch.stack([test_loader_a.dataset[i+r][1] for i in range(display_size)]).cuda()
+test_display_segs_b = torch.stack([test_loader_b.dataset[i+r][1] for i in range(display_size)]).cuda()
 
 # Setup logger and output folders
 model_name = os.path.splitext(os.path.basename(opts.config))[0]
 train_writer = tensorboardX.SummaryWriter(os.path.join(opts.output_path + "/logs", model_name))
-output_directory = os.path.join(opts.output_path + "/outputs", model_name)
+dtstr = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+output_directory = os.path.join(opts.output_path + "/outputs", model_name, dtstr)
 checkpoint_directory, image_directory = prepare_sub_folder(output_directory)
 shutil.copy(opts.config, os.path.join(output_directory, 'config.yaml')) # copy config file to output folder
 
 # Start training
 iterations = trainer.resume(checkpoint_directory, hyperparameters=config) if opts.resume else 0
 while True:
-    for it, (images_a, images_b) in enumerate(zip(train_loader_a, train_loader_b)):
+    for it, (imageseg_a, imagesseg_b) in enumerate(zip(train_loader_a, train_loader_b)):
+        images_a = imageseg_a[0]
+        segs_a = imageseg_a[1]
+        path_a = imageseg_a[2]
+
+        images_b = imagesseg_b[0]
+        segs_b = imagesseg_b[1]
+        path_b = imagesseg_b[2]
+
         trainer.update_learning_rate()
         images_a, images_b = images_a.cuda().detach(), images_b.cuda().detach()
+        segs_a, segs_b = segs_a.cuda().detach(), segs_b.cuda().detach()
 
         with Timer("Elapsed time in update: %f"):
             # Main training code
-            trainer.dis_update(images_a, images_b, config)
-            trainer.gen_update(images_a, images_b, config)
+            trainer.dis_update(images_a, segs_a, images_b, segs_b, config)
+            if 0 == iterations % 3:
+           #     trainer.gen_update(images_a, segs_a, images_b, segs_b, config, True)
+                trainer.gen_update(images_a, segs_a, images_b, segs_b, config)
             torch.cuda.synchronize()
 
         # Dump training stats in log file
@@ -74,16 +94,25 @@ while True:
         # Write images
         if (iterations + 1) % config['image_save_iter'] == 0:
             with torch.no_grad():
-                test_image_outputs = trainer.sample(test_display_images_a, test_display_images_b)
-                train_image_outputs = trainer.sample(train_display_images_a, train_display_images_b)
+                test_image_outputs = trainer.sample(test_display_images_a,
+                                                    test_display_segs_a,
+                                                    test_display_images_b,
+                                                    test_display_segs_b)
+                train_image_outputs = trainer.sample(train_display_images_a,
+                                                     train_display_segs_a,
+                                                     train_display_images_b,
+                                                     train_display_segs_b)
             write_2images(test_image_outputs, display_size, image_directory, 'test_%08d' % (iterations + 1))
             write_2images(train_image_outputs, display_size, image_directory, 'train_%08d' % (iterations + 1))
             # HTML
             write_html(output_directory + "/index.html", iterations + 1, config['image_save_iter'], 'images')
 
-        if (iterations + 1) % config['image_display_iter'] == 0:
+        if ((iterations + 1) % config['image_display_iter'] == 0) or (iterations == 100):
             with torch.no_grad():
-                image_outputs = trainer.sample(train_display_images_a, train_display_images_b)
+                image_outputs = trainer.sample(train_display_images_a,
+                                               train_display_segs_a,
+                                               train_display_images_b,
+                                               train_display_segs_b)
             write_2images(image_outputs, display_size, image_directory, 'train_current')
 
         # Save network weights
